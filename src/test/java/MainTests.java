c368b483eb20ae734628f42f825f34f1ebb99d9d
Oleg <olegchb33@gmail.com>
2022-10-24 22:15:23 +0300

    Lesson 11.verification and assertion.

--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -1,6 +1,7 @@
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -37,11 +38,25 @@ public class MainTests extends BaseUI {
     @Test
     public void testMainTabs() {
         List<WebElement> tabs = driver.findElements(Locators.TAB_LINKS);
+        String expectedSearchTitle = "Single Ukrainian women online";
+        String expectedSearchUrl = "/users/search";
+        String searchPageName = "PRETTY WOMEN";
+        String actualTitle = null;
         for (int i = 1; i < tabs.size(); i++) {
             if (tabs.get(i).isDisplayed()) {
-                String text = tabs.get(i).getText();
+                String nameOfTab = tabs.get(i).getText();
                 tabs.get(i).click();
-                System.out.println(text);
+                String currentUrl = driver.getCurrentUrl();
+                if(!nameOfTab.contains("GIFTS")){
+                    actualTitle = driver.findElement(By.xpath("//h1")).getText();
+                }
+                if(nameOfTab.contains(searchPageName)){
+                    if(!currentUrl.contains(expectedSearchUrl)){
+                        Assert.fail();
+                    }
+                    Assert.assertTrue(actualTitle.contains(expectedSearchTitle),"Title is different:" + actualTitle);
+                    Assert.assertTrue(currentUrl. contains(expectedSearchUrl), "Url is different:" + currentUrl );
+                }
                 driver.navigate().back();
                 tabs = driver.findElements(Locators.TAB_LINKS);
             }
